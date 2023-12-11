662abe44806afd8f8f01c7310d4d5e6f8f9a4853
foysalmoveon <foysal.moveon@com.bd>
2023-12-11 18:45:10 +0600

    Last update

--- src/api/index.ts
+++ src/api/index.ts
@@ -31,11 +31,12 @@ export default (rootDirectory: string) => {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use(
-    `/orders/sslcommerz-payments/:order_id`,
+    `/admin/orders/sslcommerz-payments/:order_id`,
     cors(corsOptions)
   )
 
-  app.get(`/orders/sslcommerz-payments/:order_id`, async (req, res) => {
+  app.get(`/admin/orders/sslcommerz-payments/:order_id`, async (req, res) => {
+    console.log(req);
     const { order_id } = req.params
     const sslCommerz: SSLcommerzBase = req.scope.resolve("stripeProviderService")
     const orderService: OrderService = req.scope.resolve("orderService")
@@ -53,8 +54,24 @@ export default (rootDirectory: string) => {
       customer: order.customer,
       paymentSessionData: order.metadata };
     const paymentResponse = await sslCommerz.initiatePayment(context)
-    res.json({ paymentResponse });
+    res.json(paymentResponse);
   })
+
+
+
+  app.post(`/payments/success/:tran_id`, async (req, res) => {
+    console.log(req.body)
+    res.status(200).json({data: req.body})
+  })
+
+  app.post(`/payments/failed/:tran_id`, async (req, res) => {
+    return res.status(400).json({data: req.body})
+  })
+
+  app.post(`/payments/cancel/:tran_id`, async (req, res) => {
+    return res.status(200).json({data: req.body})
+  })
+
   
   return app
 }
--- src/core/sslcommerz-base.ts
+++ src/core/sslcommerz-base.ts
@@ -92,72 +92,74 @@ abstract class SSLcommerzBase extends AbstractPaymentProcessor {
   async initiatePayment(
     context: PaymentProcessorContext
   ): Promise<PaymentProcessorError | PaymentProcessorSessionResponse> {
+    const intentRequestData = this.getPaymentIntentOptions()
+    const {
+      email,
+      currency_code,
+      amount,
+      customer,
+    } = context
+
+    this.sslInit(SSLCOMMERZ_STORE_ID, SSLCOMMERZ_STORE_SECRCT_KEY, IS_LIVE);
+    const trans_id = generateTransactionId();
+
+    const data = {
+      total_amount: amount,
+      currency: currency_code.toUpperCase(),
+      tran_id: trans_id,
+      success_url: `/sslcommerz-payments/success/${trans_id}`,
+      fail_url: `/sslcommerz-payments/fail/${trans_id}`,
+      cancel_url: `/sslcommerz-payments/cancel/${trans_id}`,
+      ipn_url: '/ipn',
+      shipping_method: 'Curireir',
+      product_name: "Computer,Speaker",
+      product_category: 'Electronic',
+      product_profile: 'general',
+      cus_name: customer?.first_name,
+      cus_email: email,
+      cus_add1: 'Dhaka',
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: null,
+      cus_country: null,
+      cus_phone: customer?.phone,
+      cus_fax: null,
+      ship_name: 'air',
+      ship_add1: "Mirpur-10",
+      ship_add2: "Mirpur-12",
+      ship_city: "Dhaka",
+      ship_state: null,
+      ship_postcode: 1254,
+      ship_country: "Dhaka",
+      capture_method: this.options_.capture ? "automatic" : "manual",
+    };
+
+    let sslCommerzResponse: { sessionKey: any };
+    
     try {
-
-      const {
-        email,
-        context: cart_context,
-        currency_code,
-        amount,
-        resource_id,
-        customer,
-      } = context
-
-      const intentRequestData = this.getPaymentIntentOptions()
-
-      await this.sslInit(SSLCOMMERZ_STORE_ID, SSLCOMMERZ_STORE_SECRCT_KEY, IS_LIVE);
-      const trans_id = generateTransactionId();
-
-      const data = {
-        total_amount: amount,
-        currency: currency_code.toUpperCase(),
-        tran_id: trans_id, // use unique tran_id for each api call
-        success_url: `http://localhost:9000/success/${trans_id}`,
-        fail_url: 'http://localhost:3030/fail',
-        cancel_url: 'http://localhost:3030/cancel',
-        ipn_url: 'http://localhost:3030/ipn',
-        shipping_method: 'Curireir',
-        product_name: "Computer,Speaker",
-        product_category: 'Electronic',
-        product_profile: 'general',
-        cus_name: customer?.first_name,
-        cus_email: customer?.email,
-        cus_add1: 'Dhaka',
-        cus_add2: 'Dhaka',
-        cus_city: 'Dhaka',
-        cus_state: 'Dhaka',
-        cus_postcode: null,
-        cus_country: null,
-        cus_phone: customer?.phone,
-        cus_fax: null,
-        ship_name: 'air',
-        ship_add1: "Mirpur-10",
-        ship_add2: "Mirpur-12",
-        ship_city: "Dhaka",
-        ship_state: null,
-        ship_postcode: 1254,
-        ship_country: "Dhaka",
-      };
-
-      
       const sslcommer = this.sslcommerce_;
-      const intent = await sslcommer.init(data);
-
-
-      let session_data = intent;
-      console.log(intent, "intent");
-
-      return {
-        session_data,
-        
-      };
-    } catch (error) {
-      console.error(error);
-      return {
-        error: 'Error initiating payment',
-      };
+      sslCommerzResponse = await sslcommer.init(data);
+    } catch (e) {
+      return this.buildError(
+        "An error occurred in initiatePayment when initiating SSLCommerz payment",
+        e
+      );
     }
+  
+    console.log(sslCommerzResponse,"sslCommerzResponse")
+    return {
+      session_data: sslCommerzResponse,
+      update_requests: customer?.metadata?.sessionkey
+        ? undefined
+        : {
+          customer_metadata: {
+            sessionkey: sslCommerzResponse.sessionKey,  // Fix the typo here
+          },
+        },
+    } as PaymentProcessorSessionResponse
   }
+
 
   async authorizePayment(
     paymentSessionData: Record<string, unknown>,
@@ -165,13 +167,38 @@ abstract class SSLcommerzBase extends AbstractPaymentProcessor {
   ): Promise<
     | PaymentProcessorError
     | {
-        status: PaymentSessionStatus
-        data: PaymentProcessorSessionResponse["session_data"]
+        status: PaymentSessionStatus;
+        data: PaymentProcessorSessionResponse["session_data"];
       }
-  > {
-    const status = await this.getPaymentStatus(paymentSessionData)
-    return { data: paymentSessionData, status }
+    > {
+    
+    console.log(context,'context')
+    
+    let data = paymentSessionData;
+    let status: PaymentSessionStatus; // Declare the 'status' variable
+
+    console.log(data, "data")
+  
+    try {
+      let sslCommerzResponse;
+  
+      const sslcommer = this.sslcommerce_;
+      sslCommerzResponse = await sslcommer.validate(data);
+  
+      // Assuming you get a status from sslCommerzResponse or context, assign it to 'status'
+      // For example:
+      status = sslCommerzResponse.status || 'success';
+    } catch (e) {
+      return this.buildError(
+        "An error occurred in initiatePayment when initiating SSLCommerz payment",
+        e
+      );
+    }
+  
+    return { data: paymentSessionData, status };
   }
+  
+
 
   async cancelPayment(
     paymentSessionData: Record<string, unknown>
@@ -180,7 +207,7 @@ abstract class SSLcommerzBase extends AbstractPaymentProcessor {
   > {
     try {
       const id = paymentSessionData.id as string
-      return (await this.stripe_.paymentIntents.cancel(
+      return (await this.sslcommerce_.cancel(
         id
       )) as unknown as PaymentProcessorSessionResponse["session_data"]
     } catch (error) {
@@ -196,10 +223,11 @@ abstract class SSLcommerzBase extends AbstractPaymentProcessor {
     paymentSessionData: Record<string, unknown>
   ): Promise<
     PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]
-  > {
+    > {
+    console.log(paymentSessionData, "paymentsesstionData")
     const id = paymentSessionData.id as string
     try {
-      const intent = await this.stripe_.paymentIntents.capture(id)
+      const intent = await this.sslcommerce_.paymentIntents.capture(id)
       return intent as unknown as PaymentProcessorSessionResponse["session_data"]
     } catch (error) {
       if (error.code === ErrorCodes.PAYMENT_INTENT_UNEXPECTED_STATE) {
@@ -225,12 +253,14 @@ abstract class SSLcommerzBase extends AbstractPaymentProcessor {
     refundAmount: number
   ): Promise<
     PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]
-  > {
+    > {
+    console.log( paymentSessionData,"paymentSessionData")
     const id = paymentSessionData.id as string
+   
 
     try {
-      await this.stripe_.refunds.create({
-        amount: Math.round(refundAmount),
+      await this.sslcommerce_.initiateRefund({
+        amount: refundAmount,
         payment_intent: id as string,
       })
     } catch (e) {
